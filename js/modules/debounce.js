c7c6ae028235ec17cae87a6f841ad80a7be6c64b
“Pedro <pedrohenriquesampaiodenovaes@gmail.com>
2022-06-04 14:34:03 -0300

    Retirado console.log do debounce

--- js/modules/debounce.js
+++ js/modules/debounce.js
@@ -2,7 +2,6 @@ export default function debounce(callback, delay) {
   let timer;
 
   return (...args) => {
-    console.log(args)
     if (timer) clearTimeout(timer);
     timer = setTimeout(() => {
       callback(...args);
